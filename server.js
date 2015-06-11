f47027c3ff5cb6b22d4453b1842d96d7393e6dd8
scarlet9 <jimkoo9x@gmail.com>
2015-06-11 14:47:59 +0900

    remove app.configure, add body-parser module
    for express 4+

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express')
   , app = express()
+  , bodyParser = require('body-parser')
   , fs = require('fs')
   , url = require('url')
   , i18n = require('i18next');
@@ -15,22 +16,16 @@ i18n.init({
 });
 
 // Configuration
-app.configure(function() {
-    app.use(express.bodyParser());
-    app.use(i18n.handle); // have i18n befor app.router
-    
-    app.use(app.router);
-    // app.set('view engine', 'jade');
-    // app.set('views', __dirname);
+app.use(bodyParser.json());
+app.use(i18n.handle); // have i18n befor app.router
 
-    app.use('/app', express.static('client/app'));
-    app.use('/assets', express.static('client/assets'));
-    app.use('/app/templates', express.static('client/assets/templates'));
+app.use('/app', express.static('client/app'));
+app.use('/assets', express.static('client/assets'));
+app.use('/app/templates', express.static('client/assets/templates'));
 
-    // for release 
-    app.use('/release', express.static('client/dist/release/assets'));
-    app.use('/', express.static('client/dist/release/assets'));
-});
+// for release 
+app.use('/release', express.static('client/dist/release/assets'));
+app.use('/', express.static('client/dist/release/assets'));
 
 app.get("/", function(req, res) {
     return res.sendfile('index.html');
